59362c6544d2f033ddc9a1e049a98a0d878ed621
yaniv knoll <Yanivx4x@gmail.com>
2018-05-30 14:34:20 +0300

    updated channels to hold also geometry , avatar and projection

--- app.js
+++ app.js
@@ -21,9 +21,19 @@ remoteHelperNameSpace.on('connection', function (socket) {
         console.log('a remote helper disconnected');
     });
 
-    socket.on('data', function (data) {
-        console.log("sending date to local user from remote helper!");
-        localUserNameSpace.emit('data', JSON.stringify(data));
+    socket.on('geometry', function (data) {
+        console.log("sending geometry to local user from remote helper!");
+        localUserNameSpace.emit('geometry', JSON.stringify(data));
+    });
+
+    socket.on('avatar', function (data) {
+        console.log("sending avatar to local user from remote helper!");
+        localUserNameSpace.emit('avatar', JSON.stringify(data));
+    });
+
+    socket.on('projection', function (data) {
+        console.log("sending projection to local user from remote helper!");
+        localUserNameSpace.emit('projection', JSON.stringify(data));
     });
 });
 
@@ -34,9 +44,18 @@ localUserNameSpace.on('connection', function (socket) {
         console.log('a local user disconnected');
     });
 
-    socket.on('data', function (data) {
-        console.log("sending date to remote helper from local user!");
-        remoteHelperNameSpace.emit('data', JSON.stringify(data));
+    socket.on('avatar', function (data) {
+        console.log("sending avatar to remote helper from local user!");
+        remoteHelperNameSpace.emit('avatar', JSON.stringify(data));
+    });
+    socket.on('geometry', function (data) {
+        console.log("sending geometry to remote helper from local user!");
+        remoteHelperNameSpace.emit('geometry', JSON.stringify(data));
+    });
+
+    socket.on('projection', function (data) {
+        console.log("sending projection to remote helper from local user!");
+        remoteHelperNameSpace.emit('projection', JSON.stringify(data));
     });
 });
 
